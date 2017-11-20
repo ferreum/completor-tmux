7105a926df6c0348cbc30ba2e595e7bd2673fcdd
Daniel K <code.danielk@gmail.com>
2017-11-20 22:23:18 +0100

    use grep -o to split words if available

--- pythonx/completor_tmux.py
+++ pythonx/completor_tmux.py
@@ -19,30 +19,45 @@ def _escape_grep_regex(s):
     return s.translate(_grep_esc_table)
 
 
-_have_gnu_xargs_result = None
-def _have_gnu_xargs():
-    global _have_gnu_xargs_result
+class CheckFeature(object):
 
-    have = _have_gnu_xargs_result
-    if have is not None:
-        return have
+    def __init__(self, name, command, input, expect):
+        self.name = name
+        self.command = command
+        self.input = input
+        self.expect = expect
+        self.have = None
 
-    proc = subprocess.run(['xargs', '-r', '-P0', 'echo'],
-                            input=b'args_work',
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+    def __bool__(self):
+        if self.have is not None:
+            return self.have
 
-    have = proc.stdout.startswith(b'args_work')
-    _have_gnu_xargs_result = have
+        proc = subprocess.run(self.command,
+                              input=self.input,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
 
-    if have:
-        logger.info("tmux: detected gnu-compatible xargs")
-    else:
-        logger.info("tmux: no gnu compatible xargs. "
-                    "status=%r, stdout=%r, stderr=%r",
-                    proc.returncode, proc.stdout, proc.stderr)
+        self.have = self.expect(proc.stdout)
 
-    return have
+        if self.have:
+            logger.info("tmux: detected %s", self.name)
+        else:
+            logger.info("tmux: no %s found. status=%r, stdout=%r, stderr=%r",
+                        self.name, proc.returncode, proc.stdout, proc.stderr)
+
+        return self.have
+
+
+_have_gnu_xargs = CheckFeature("gnu-compatible xargs",
+                               ['xargs', '-r', '-P0', 'echo'],
+                               b'args_work',
+                               lambda b: b.startswith(b'args_work'))
+
+
+_have_grep_dash_o = CheckFeature("grep -o support",
+                                 ['grep', '-o', r'x\w\+'],
+                                 b'xy 1234 xz',
+                                 b'xy\nxz\n'.__eq__)
 
 
 def _get_script(pattern, grep_args='', exclude_pane=None):
@@ -53,12 +68,15 @@ def _get_script(pattern, grep_args='', exclude_pane=None):
         escaped = _escape_grep_regex(exclude_pane)
         s += ' | grep -v ' + shlex.quote('^' + escaped + "$")
     # capture panes
-    if _have_gnu_xargs():
+    if _have_gnu_xargs:
         s += " | xargs -r -P0 -n1 tmux capture-pane -J -p -t"
     else:
         s += " | xargs -n1 tmux capture-pane -J -p -t"
-    # split words
-    s += " | tr -c -s 'a-zA-Z0-9_' '\\n'"
+    if _have_grep_dash_o:
+        s += r" | grep -o '\w\+'"
+    else:
+        # split words
+        s += r" | tr -c -s 'a-zA-Z0-9_' '\n'"
     # filter out words not beginning with pattern
     s += ' | grep ' + grep_args + ' -- ' + shlex.quote(pattern)
     return s
@@ -93,11 +111,15 @@ class Tmux(Completor):
     sync = True
 
     def parse(self, base):
-        if len(base) < 3:
-            return []
-        this_pane = os.getenv('TMUX_PANE')
+        try:
+            if len(base) < 3:
+                return []
+            this_pane = os.getenv('TMUX_PANE')
 
-        res = _get_completions(base, exclude_pane=this_pane)
+            res = _get_completions(base, exclude_pane=this_pane)
 
-        return [{'word': token.decode('utf-8'), 'menu': '[TMUX]'}
-                for token in res]
+            return [{'word': token.decode('utf-8'), 'menu': '[TMUX]'}
+                    for token in res]
+        except Exception as e:
+            logger.exception(e)
+            raise
